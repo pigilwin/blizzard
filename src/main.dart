7946e6373cf9687b7afc778fe2e901200c802823
Tim Remnant <timremnant@Tims-MacBook-Pro.local>
2019-11-25 21:40:42 +0000

    Creating a new instance for testing purposes

--- src/main.dart
+++ src/main.dart
@@ -1,3 +1,11 @@
+import 'Blizzard.dart';
+import 'BlizzardConfiguration.dart';
+
 void main(List<String> args) {
-  
+
+  BlizzardConfiguration configuration = BlizzardConfiguration();
+
+  Blizzard blizzard = Blizzard(configuration);
+
+  blizzard.start();
 }
