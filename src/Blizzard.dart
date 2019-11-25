fce77e055336e7d1f90d0704414c67ae97ac5e7d
Tim Remnant <timremnant@Tims-MacBook-Pro.local>
2019-11-25 23:26:54 +0000

    Only setting up the instances once

--- src/Blizzard.dart
+++ src/Blizzard.dart
@@ -3,13 +3,14 @@ import 'BlizzardFlake.dart';
 
 class Blizzard {
 
-  final int amountOfFlakes = 100;
+  final int amountOfFlakes = 600;
 
-  List<BlizzardFlake> _flakes;
+  List<BlizzardFlake> _flakes = [];
   CanvasElement _canvasElement;
   CanvasRenderingContext2D _canvasRenderingContext2D;
   int innerWidth = 0;
   int innerHeight = 0;
+  bool hasBeenSetup = false;
   
   Blizzard(){
     this.innerHeight = window.innerHeight;
@@ -18,7 +19,20 @@ class Blizzard {
   }
 
   void start() {
+    if (!this.hasBeenSetup) {
+      this._setUp();
+    }
+  }
 
+  void _setUp() {
+    if (this.hasBeenSetup) {
+      return;
+    }
+    for(int i = 0; i < this.amountOfFlakes; i++) {
+      BlizzardFlake flake = BlizzardFlake();
+      this._flakes.add(flake);
+    }
+    this.hasBeenSetup = true;
   }
 
   void _attachCanvasToDocumentBody() {
