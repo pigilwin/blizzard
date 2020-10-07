c1455e069fecbb6a160d8279ff246308b07a932e
Tim Remnant <timremnant@Tims-MacBook-Pro.local>
2020-10-07 12:07:14 +0100

    Canvas is now split from main file

---
+++ src/canvas.ts
@@ -0,0 +1,71 @@
+export class Canvas {
+
+    private readonly canvasId: string = 'blizzard-canvas';
+
+    private canvas: HTMLCanvasElement;
+    private _context: CanvasRenderingContext2D;
+    private _width: number;
+    private _height: number;
+    
+    /**
+     * Build a new canvas
+     * @param {number} height 
+     * @param {number} width 
+     */
+    public constructor(height: number, width: number) {
+        this._height = height;
+        this._width = width;
+
+        /**
+         * If the element is already detected then lets fail
+         */
+        if (document.querySelector("#" + this.canvasId) !== null) {
+            throw new Error('A blizzard canvas element has already been found');
+        }
+
+        this.canvas = document.createElement('canvas');
+        this.canvas.id = 'blizzard-canvas';
+        this._context = <CanvasRenderingContext2D> this.canvas.getContext('2d');
+        this.style();
+        document.body.appendChild(this.canvas);
+    }
+
+    /**
+     * Fetch the context of the canvas
+     * @returns {CanvasRenderingContext2D}
+     */
+    public get context(): CanvasRenderingContext2D {
+        return this._context;
+    }
+
+    public get height(): number {
+        return this._height;
+    }
+
+    public set height(n: number) {
+        this._height = n;
+    }
+
+    public get width(): number {
+        return this._width;
+    }
+
+    public set width(n: number) {
+       this._width = n;
+    }
+
+    /**
+     * Set the style
+     */
+    private style(): void {
+        this.canvas.height = this._height;
+        this.canvas.width = this._width;
+        this.canvas.style.margin = '0';
+        this.canvas.style.padding = '0';
+        this.canvas.style.position = 'fixed';
+        this.canvas.style.touchAction = 'none';
+        this.canvas.style.top = '0';
+        this.canvas.style.left = '0';
+        this.canvas.style.zIndex = '-1';
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,22 @@
 import {BlizzardConfigurationItem} from './types';
 import {Helpers} from './helpers';
 import {Flake} from './flake';
+import {Canvas} from './canvas';
 
 export default class Blizzard {
+
     private configuration: BlizzardConfigurationItem;
+    private canvas: Canvas;
     private flakes: Array<Flake> = [];
-    private canvas: HTMLCanvasElement;
-    private context: CanvasRenderingContext2D;
-    private windowWidth = 0;
-    private windowHeight = 0;
-    private loaded = false;
-    private mouseX = -100;
 
-    public constructor(private config: BlizzardConfigurationItem = {}) {
+    /**
+     * Create a new blizzard
+     * @param {BlizzardConfigurationItem} config 
+     */
+    public constructor(config: BlizzardConfigurationItem = {}) {
         this.configuration = Object.assign(this.defaultConfiguration(), config);
-        this.windowHeight = window.innerHeight;
-        this.windowWidth = window.innerWidth;
-        this.canvas = document.createElement('canvas');
-        this.context = <CanvasRenderingContext2D> this.canvas.getContext('2d');
-        this.setCanvasStyle();
-        document.body.appendChild(this.canvas);
-
-        if (!this.loaded) {
-            this.load();
-        }
+        this.canvas = new Canvas(window.innerHeight, window.innerWidth);
+        this.load();
     } 
 
     /**
@@ -40,19 +33,13 @@ export default class Blizzard {
      * @returns {void}
      */
     private load(): void {
-        if (this.loaded) {
-            return;
-        }
-
         let i: number = this.configuration.flakeCount;
 
         while(i--) {
-            const x: number = Helpers.random(0, this.windowWidth, true);
-            const y: number = Helpers.random(0, this.windowHeight, true);
+            const x: number = Helpers.random(0, this.canvas.width, true);
+            const y: number = Helpers.random(0, this.canvas.width, true);
             this.flakes.push(new Flake(x, y));
         }
-
-        this.loaded = true;
     }
 
     /**
@@ -60,8 +47,8 @@ export default class Blizzard {
      * @returns {void}
      */
     public scaleCanvas(): void {
-        this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
+        this.canvas.width = window.innerWidth;
     }
 
     /**
@@ -70,7 +57,7 @@ export default class Blizzard {
      * @returns {void} 
      */
     public applyMouseEvent(e: MouseEvent): void {
-        this.mouseX = e.clientX;
+        //this.mouseX = e.clientX;
     }
 
     /**
@@ -81,10 +68,10 @@ export default class Blizzard {
 
         let i: number = this.flakes.length;
 
-        this.context.save();
-        this.context.setTransform(1, 0, 0, 1, 0, 0);
-        this.context.clearRect(0, 0, window.innerWidth, window.innerHeight);
-        this.context.restore();
+        this.canvas.context.save();
+        this.canvas.context.setTransform(1, 0, 0, 1, 0, 0);
+        this.canvas.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.canvas.context.restore();
 
         while(i--) {
             const flake: Flake = this.flakes[i];
@@ -104,30 +91,17 @@ export default class Blizzard {
             this.loop();
         });
     }
-    
-    private setCanvasStyle(): void {
-        this.canvas.height = this.windowHeight;
-        this.canvas.width = this.windowWidth;
-        this.canvas.style.margin = '0';
-        this.canvas.style.padding = '0';
-        this.canvas.style.position = 'fixed';
-        this.canvas.style.touchAction = 'none';
-        this.canvas.style.top = '0';
-        this.canvas.style.left = '0';
-        this.canvas.style.zIndex = '-1';
-    }
 
     /**
      * Draw a character
-     * @param x 
-     * @param y 
-     * @param flake 
+     * @param {number} x 
+     * @param {number} y
      * @returns {void}
      */
     private drawCharacterFlake(x: number, y: number): void
     {
-        this.context.font = '2em serif';
-        this.context.fillText(this.configuration.flakeCharacter, x, y);
+        this.canvas.context.font = '2em serif';
+        this.canvas.context.fillText(this.configuration.flakeCharacter, x, y);
     }
 
     /**
