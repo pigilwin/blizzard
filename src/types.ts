f439ac4907ab76bd11e83761de559cda2caf601e
Tim Remnant <timremnant@Tims-MacBook-Pro.local>
2020-10-07 10:53:59 +0100

    Adding comments

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,21 @@
 export interface BlizzardConfigurationItem {
+    /**
+     * How many flakes should be shown on the page at one time
+     */
     flakeCount: number;
+
+    /**
+     * What is the virtual wind speed of the application
+     */
     windSpeed: number;
-    red: number;
-    green: number;
-    blue: number;
+
+    /**
+     * What character should be shown when displaying the flake
+     */
     flakeCharacter: string;
+
+    /**
+     * Should the snow flakes react to the mouse
+     */
     avoidMouse: boolean;
 }
