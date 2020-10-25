65a57f34dd4241c0b7be1ae1655f8a50b1b0102d
Tim Remnant <timremnant@unknown-38:f9:d3:a6:82:b6.lan>
2020-10-25 10:26:53 +0000

    Correct random number generation

--- src/helpers.ts
+++ src/helpers.ts
@@ -8,7 +8,7 @@ export class Helpers {
      * @returns {number}
      */
     public static random(min: number, max: number, round: boolean): number {
-        const generatedNumber: number = Math.random() * (max - min + 1) + 1;
+        const generatedNumber: number = Math.random() * (max - min + 1) + min;
         if (round) {
             return Math.floor(generatedNumber);
         }
