8f873802fab90cd132de4ba0aa9dc0ab3e27d4ec
Tim Remnant <timwebdesign2013@gmail.com>
2021-12-18 19:05:50 +0000

    Swapping to production and development config files

---
+++ rollup.config.production.js
@@ -0,0 +1,24 @@
+import typescriptPlugin from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+import typescript from 'typescript';
+
+export default {
+    input: 'src/index.ts',
+    output: [
+        {
+            file: pkg.main,
+            format: 'iife',
+            name: 'Blizzard'
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+            name: 'Blizzard'
+        },
+    ],
+    plugins: [
+        typescriptPlugin({
+          typescript: typescript,
+        })
+    ]
+};
